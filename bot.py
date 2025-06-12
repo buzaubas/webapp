60bd8ff4fc01dce529e9da4f65e6571f999b53ac
Dimash <beks.dimash@mail.ru>
2025-06-12 12:06:24 +0500

    Added start to boy.py

--- bot.py
+++ bot.py
@@ -36,3 +36,30 @@ async def main():
     
 if __name__ == "__main__":
     asyncio.run(main())
+
+# from aiogram import Bot, Dispatcher, types
+# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+# from aiogram.utils import executor
+# import logging
+# import os
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    web_app_url = "https://webapp-ox1d.onrender.com/"  # Заменить на свой адрес
+
+    keyboard = InlineKeyboardMarkup().add(
+        InlineKeyboardButton(
+            text="Открыть WebApp",
+            web_app=WebAppInfo(url=web_app_url)
+        )
+    )
+
+    await message.answer("Нажми кнопку ниже, чтобы открыть WebApp:", reply_markup=keyboard)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
